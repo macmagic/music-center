8284b2460d74721c875bfe20705be407e345d183
Juan Arroyes <arroyes89@gmail.com>
2020-09-06 22:50:45 +0200

    Basic libraries and infra

---
+++ src/test/java/com/juanarroyes/musiccenter/MusicCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanarroyes.musiccenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
